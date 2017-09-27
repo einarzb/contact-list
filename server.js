4a4877c793343ce731722ec8f21afc34be5c04e2
einarzb <einarzb@gmail.com>
2017-09-27 15:49:44 +0300

    initliazed basic server using npm packages, and also initialized app, ctrler and service files

---
+++ server.js
@@ -0,0 +1,35 @@
+
+//npm packages requirements
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var port = 3000;
+var app = express();
+
+//middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static('public'));
+app.use(express.static('node_modules'));
+
+
+//404 error
+app.use(function(req, res, next){
+  var err = new Error('Not found');
+  err.status = 404;
+  next(err);
+});
+
+// main error handler
+// warning - not for use in production code!
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.send({
+    message: err.message,
+    error: err
+  });
+});
+
+app.listen(port, function() {
+  console.log("trainologic test. rockenroll on " + port)
+});
